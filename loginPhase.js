a91278dbb4cee13e5298eb2437b92b4f2a3f5d0f
Inigo Joints <madrigalinigojones@gmail.com>
2026-03-05 23:06:59 +0800

    Added some minor changes and additions

---
+++ loginPhase.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".scroll-reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (elementTop < windowHeight - revealPoint) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
