fd853d7bb8c2d9271d6233be2cc43b08a734baaa
Inigo Joints <madrigalinigojones@gmail.com>
2026-03-06 22:51:03 +0800

    Close to completing the loginPhase

---
+++ starts.php
@@ -0,0 +1,45 @@
+<?php
+include "kapstongConnection.php";
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+if (isset($_POST['login-submit'])) {
+    $userEmail = $_POST["loginEmail"];
+    $userPassword = $_POST["loginPassword"];
+
+    $sql_prep = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $sql_prep->bind_param("s", $userEmail);
+    $sql_prep->execute();
+
+    $result = $sql_prep->get_result();
+
+    if ($result->num_rows > 0) {
+
+        $row = $result->fetch_assoc();
+
+        if (password_verify($userPassword, $row['password'])) {
+
+            session_start();
+
+            $_SESSION['user_id'] = $row['userID'];
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['role'] = 'student';
+            $_SESSION['isVerified'] = $row['isVerified'];
+            $_SESSION['mobileNumber'] = $row['mobileNumber'];
+
+            header("Location: trackerMain.php?login=Logging In..");
+            exit();
+        } else {
+
+            header("Location: loginPhase.php?error=Incorrect+Password");
+            exit();
+        }
+    } else {
+
+        header("Location: loginPhase.php?error=ID+not+Found");
+        exit();
+    }
+}
